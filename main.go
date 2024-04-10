4a891429465c2927bf2a191a04e584fc4302ef75
m.siemieniaka <marcin.siemieniaka@gmail.com>
2024-04-10 20:11:27 +0200

    Get/Post Events

--- main.go
+++ main.go
@@ -3,16 +3,35 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"rest-api/app/models"
 )
 
 func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080") //localhost:8080
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"Message": "Hello!"})
+	events := models.GetAllEvent()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not post data"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
 }
