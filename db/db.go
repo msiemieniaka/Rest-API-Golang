4fcac8f88af0771009a9314c27ebc60f703e8363
m.siemieniaka <marcin.siemieniaka@gmail.com>
2024-04-11 16:52:59 +0200

    user Tables update

--- db/db.go
+++ db/db.go
@@ -22,6 +22,20 @@ func InitDB() {
 }
 
 func createTables() {
+	createUserTable := `
+	CREATE TABLE IF NOT EXISTS users (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	email TEXT NOT NULL UNIQUE,
+	password TEXT NOT NULL
+)
+`
+_, err := DB.Exec(createUserTable)
+
+if err != nil {
+	panic("Could not create user table")
+}
+
+
 	createEventsTable := `
 	CREATE TABLE IF NOT EXISTS events (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -33,7 +47,7 @@ func createTables() {
 	)
 	`
 
-	_, err := DB.Exec(createEventsTable)
+	_, err = DB.Exec(createEventsTable)
 
 	if err != nil {
 		panic("Could not create events table")
