dc72ac5fb3541e97ec9b92e4b91b4626c7638c05
m.siemieniaka <marcin.siemieniaka@gmail.com>
2024-04-11 17:17:46 +0200

    password hash

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "rest-api/app/db"
+import (
+	"rest-api/app/db"
+	"rest-api/app/utils"
+)
 
 type User struct {
 	ID int64
@@ -18,7 +21,13 @@ func (u User) Save() error {
 	
 	defer stmt.Close()
 	
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 	
 	if err != nil {
 		return err
