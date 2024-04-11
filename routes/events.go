e252d8cbb5e170f05d74a13cdc410a011f65f11f
m.siemieniaka <marcin.siemieniaka@gmail.com>
2024-04-11 16:47:52 +0200

    Update/Delete events handler

--- routes/events.go
+++ routes/events.go
@@ -51,3 +51,56 @@ func createEvent(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
 }
+
+func updateEvent(context *gin.Context) {
+	eventID, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse single event"})
+		return
+	}
+
+	_, err = models.GetEventByID(eventID)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not fetch single event"})
+		return
+	}
+
+	var updatedEvent models.Event
+	err = context.ShouldBindJSON(&updatedEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not post data"})
+		return
+	}
+	updatedEvent.ID = eventID
+	updatedEvent.Update()
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not update event"})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Sucesfully updated an event"})
+}
+
+func deleteEvents(context *gin.Context) {
+	eventID, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse single event"})
+		return
+	}
+
+	event, err := models.GetEventByID(eventID)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not fetch single event"})
+		return
+	}
+
+	err = event.Delete()
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not delete event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Sucesfully deleted an event"})
+}
